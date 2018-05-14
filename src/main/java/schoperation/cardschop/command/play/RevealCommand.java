ee8639378cef23dfc1f3a5b15c0f339c355c6788
Schoperation <cschop1@gmail.com>
2018-05-14 18:09:57 -0400

    You can now reveal your cards to someone else privately.

--- src/main/java/schoperation/cardschop/command/play/RevealCommand.java
+++ src/main/java/schoperation/cardschop/command/play/RevealCommand.java
@@ -6,6 +6,7 @@ import schoperation.cardschop.util.Msges;
 import schoperation.cardschop.util.Utils;
 import sx.blah.discord.handle.obj.IChannel;
 import sx.blah.discord.handle.obj.IGuild;
+import sx.blah.discord.handle.obj.IMessage;
 import sx.blah.discord.handle.obj.IUser;
 
 public class RevealCommand implements ICommand {
@@ -13,12 +14,15 @@ public class RevealCommand implements ICommand {
     /*
         Reveals something of yours.
 
-        reveal -> reveals something to EVERYONE.
+        reveal [something] -> reveals something to EVERYONE.
+        reveal [something] [someone] -> reveals something to a specific person.
 
         Options include:
             -hand -> your hand. This is default, if no arguments are provided.
             -pile -> your personal pile.
             -infront -> the cards in front of you.
+
+        Mention the person for revealing something to just them.
      */
 
     private final String command = "reveal";
@@ -39,11 +43,104 @@ public class RevealCommand implements ICommand {
 
             // What place?
             if (arg1.equals("blank") || arg1.equals("hand"))
-                channel.sendMessage(sender.getDisplayName(guild) + "'s hand:\n" + player.handToString());
+            {
+                // Is someone specified?
+                if (arg2.equals("blank"))
+                    channel.sendMessage(sender.getDisplayName(guild) + "'s hand:\n" + player.handToString());
+                else
+                {
+                    // Player they're going to give it to.
+                    // Make sure they are part of a table first.
+                    arg2 = arg2.replaceAll("[<>@!]", "");
+                    IUser userFromString = guild.getUserByID(Long.parseLong(arg2));
+
+                    // Is this player part of the table?
+                    if (Utils.isPartOfTable(userFromString, guild))
+                    {
+                        Player player2 = Utils.getPlayerObj(userFromString, guild);
+
+                        if (player2.getTable() == player.getTable())
+                        {
+                            player2.getPM().edit(sender.getDisplayName(guild) + "'s hand:\n" + player.handToString());
+                            IMessage msg = player2.getUser().getOrCreatePMChannel().sendMessage(sender.getDisplayName(guild) + " has revealed their hand to you.");
+                            msg.delete();
+                            return;
+                        }
+
+                        channel.sendMessage(userFromString.getDisplayName(guild) + " is not part of this table!");
+                        return;
+                    }
+
+                    channel.sendMessage(userFromString.getDisplayName(guild) + " is not part of this table!");
+                    return;
+                }
+            }
             else if (arg1.equals("pile"))
-                channel.sendMessage(sender.getDisplayName(guild) + "'s side pile:\n" + player.pileToString());
+            {
+                // Is someone specified?
+                if (arg2.equals("blank"))
+                    channel.sendMessage(sender.getDisplayName(guild) + "'s side pile:\n" + player.pileToString());
+                else
+                {
+                    // Player they're going to give it to.
+                    // Make sure they are part of a table first.
+                    arg2 = arg2.replaceAll("[<>@!]", "");
+                    IUser userFromString = guild.getUserByID(Long.parseLong(arg2));
+
+                    // Is this player part of the table?
+                    if (Utils.isPartOfTable(userFromString, guild))
+                    {
+                        Player player2 = Utils.getPlayerObj(userFromString, guild);
+
+                        if (player2.getTable() == player.getTable())
+                        {
+                            player2.getPM().edit(sender.getDisplayName(guild) + "'s side pile:\n" + player.pileToString());
+                            IMessage msg = player2.getUser().getOrCreatePMChannel().sendMessage(sender.getDisplayName(guild) + " has revealed their personal pile to you.");
+                            msg.delete();
+                            return;
+                        }
+
+                        channel.sendMessage(userFromString.getDisplayName(guild) + " is not part of this table!");
+                        return;
+                    }
+
+                    channel.sendMessage(userFromString.getDisplayName(guild) + " is not part of this table!");
+                    return;
+                }
+            }
             else if (arg1.equals("infront") || arg1.equals("trick"))
-                channel.sendMessage(sender.getDisplayName(guild) + "'s front cards:\n" + player.frontToString());
+            {
+                // Is someone specified?
+                if (arg2.equals("blank"))
+                    channel.sendMessage(sender.getDisplayName(guild) + "'s front cards:\n" + player.frontToString());
+                else
+                {
+                    // Player they're going to give it to.
+                    // Make sure they are part of a table first.
+                    arg2 = arg2.replaceAll("[<>@!]", "");
+                    IUser userFromString = guild.getUserByID(Long.parseLong(arg2));
+
+                    // Is this player part of the table?
+                    if (Utils.isPartOfTable(userFromString, guild))
+                    {
+                        Player player2 = Utils.getPlayerObj(userFromString, guild);
+
+                        if (player2.getTable() == player.getTable())
+                        {
+                            player2.getPM().edit(sender.getDisplayName(guild) + "'s front cards:\n" + player.frontToString());
+                            IMessage msg = player2.getUser().getOrCreatePMChannel().sendMessage(sender.getDisplayName(guild) + " has revealed their front cards to you.");
+                            msg.delete();
+                            return;
+                        }
+
+                        channel.sendMessage(userFromString.getDisplayName(guild) + " is not part of this table!");
+                        return;
+                    }
+
+                    channel.sendMessage(userFromString.getDisplayName(guild) + " is not part of this table!");
+                    return;
+                }
+            }
             else if (arg1.equals("middle"))
             {
                 // Must be the dealer to reveal the middle.
